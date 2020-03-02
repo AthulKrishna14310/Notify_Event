b58355687684155fda5ce50128ae41b95088837c
midhunpaul396 <midhunpaul396@gmail.com>
2020-03-02 14:59:47 +0530

    Ui partially done

--- app/src/main/java/com/example/notify_event/MainActivity.java
+++ app/src/main/java/com/example/notify_event/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
     }
 }
